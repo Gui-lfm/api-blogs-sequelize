70428c771d2cc52950c0dca185d3c4cfaea7da87
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-05-11 18:57:22 -0300

    requisito 01

---
+++ src/migrations/20230511203428-createUsers.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @param {import('sequelize').QueryInterface} queryInterface
+ * @param {*} Sequelize
+ */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      display_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('users');
+  },
+};
