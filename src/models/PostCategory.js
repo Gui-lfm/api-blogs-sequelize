56e443383ab2a851541208ab0fd027028594e21c
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-05-15 16:36:08 -0300

    requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,42 @@
+/**
+ *
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ */
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    'PostCategory',
+    {
+      postId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
+      categoryId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'posts_categories',
+    }
+  );
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'BlogPost',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'Category',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  };
+
+  return PostCategory;
+};
